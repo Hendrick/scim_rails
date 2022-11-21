6ad43bc3be919757b40e9f5a5605099f772e28b5
Landon Gray <landon.gray@testdouble.com>
2022-11-21 13:41:27 -0500

    Cleanup unit tests, seperate tests that use env config from the tests that use the company model

--- spec/controllers/scim_rails/scim_users_request_spec.rb
+++ spec/controllers/scim_rails/scim_users_request_spec.rb
@@ -25,34 +25,6 @@ RSpec.describe ScimRails::ScimUsersController, type: :request do
 
   context "Basic Authorization" do
 
-    context "with Company model table" do
-      let(:company) { create(:company) }
-      let(:credentials) { Base64::encode64("#{company.subdomain}:#{company.api_token}") }
-      let(:authorization) { "Basic #{credentials}" }
-      describe "Content-Type" do
-        it "accepts scim+json" do
-          expect(company.users.count).to eq 0
-
-          post_request("application/scim+json")
-
-          expect(request.params).to include :name
-          expect(response.status).to eq 201
-          expect(response.media_type).to eq "application/scim+json"
-          expect(company.users.count).to eq 1
-        end
-
-        it "can not parse unfamiliar content types" do
-          expect(company.users.count).to eq 0
-
-          post_request("text/csv")
-
-          expect(request.params).not_to include :name
-          expect(response.status).to eq 422
-          expect(company.users.count).to eq 0
-        end
-      end
-    end
-
     context "with app SCIM authentication ENV variables set" do
 
       before do
@@ -86,7 +58,6 @@ RSpec.describe ScimRails::ScimUsersController, type: :request do
           expect(User.count).to eq 0
         end
       end
-
 
       after do
         ENV['SCIM_USERNAME'] = @cached_subdomain
